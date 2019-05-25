58220e1aa224858e362b4714ab7896b268bdc3b6
Akinlabi Ajelabi <akinlabiajelabi@Akinlabis-MacBook-Pro.local>
2019-05-25 05:36:44 +0100

    first commit

---
+++ user/src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+// config
+
+// components
+
+// images
+import logo from '../images/logo.svg';
+
+// styles
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          This is the <span className='lead'>USER</span> frontend application...
+        </h1>
+        <p>
+          Happy Coding!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
